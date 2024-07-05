bfb450e4708594291701d13cdcdb7c39d1711a0d
Marcin Kozlowski <marcinkozlowski390@gmail.com>
2024-07-05 22:28:37 +0200

    date saved as new const

--- src/Container/Clock/index.js
+++ src/Container/Clock/index.js
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import "./style.css";
 
+const formatDate = (date) =>
+  date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long",
+  });
+
 export const Clock = () => {
   const [date, setDate] = useState(new Date());
 
@@ -14,17 +24,5 @@ export const Clock = () => {
     };
   }, []);
 
-  return (
-    <div className="clock">
-      Dzisiaj jest{" "}
-      {date.toLocaleString(undefined, {
-        weekday: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        day: "numeric",
-        month: "long",
-      })}
-    </div>
-  );
+  return <div className="clock">Dzisiaj jest {formatDate(date)}</div>;
 };
