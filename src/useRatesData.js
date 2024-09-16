c75b373b8d12339e5d0a675c87ca6a377f7c5180
Marcin Kozlowski <marcinkozlowski390@gmail.com>
2024-09-16 12:03:25 +0200

    useRatesData created

---
+++ src/useRatesData.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export const useRatesData = () => {
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
+
+  useEffect(() => {
+    const fetchRates = async () => {
+      try {
+        const response = await fetch(
+          "https://api.currencyapi.com/v3/latest?apikey=cur_live_x8vKi7iE8zCeHSkdXdSOFd5uvwEABaaxczJDsPG4&currencies=EUR%2CUSD%2CGBP%2CCHF&base_currency=PLN"
+        );
+
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const { data, date } = await response.json();
+
+        setRatesData({
+          state: "success",
+          data,
+          date,
+        });
+      } catch {
+        setRatesData({
+          state: "error",
+        });
+      }
+    };
+    setTimeout(fetchRates, 1000);
+  }, []);
+
+  return ratesData;
+};
